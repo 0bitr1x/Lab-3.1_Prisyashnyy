02888eb646cf6617b4b22b8df1b1721da5991f4d
prisy <prisy@LAPTOP-23R98U4U>
2023-10-30 00:02:06 +0400

    Добавьте файлы проекта.

---
+++ Lab№3.1_Prisyashnyy/Program.cs
@@ -0,0 +1,20 @@
+﻿//Сборник задач/тема 3/подтема 3.1/средний уровень/вариант 6
+try
+{
+    Console.WriteLine("Введите k:");
+    double k = double.Parse(Console.ReadLine());
+    double w = 0;
+    for (double i = -2; i <= k; i++)
+    {
+        double factorial = i + 3;
+        double f = factorial;
+        for (double j = 1; j <= f; j++) factorial *= j;
+        w += (Math.Pow(-1, i) * factorial) / (i - 4);
+    }
+    Console.WriteLine($"W = {w}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
